4cc77376c874fd132780c98216fd69ac5702da13
pavel <pavelsergmgtu@gmail.com>
2025-04-17 00:27:07 +0500

    front part

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/header', name: 'app_index_header')]
+    public function header(): Response
+    {
+        return $this->render('_header.html.twig', []);
+    }
+}
